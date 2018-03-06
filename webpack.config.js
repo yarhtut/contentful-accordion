daa1f7b70f326cbf900ccd667398393ab1f5bf3e
yarhtut <yar@boost.co.nz>
2018-03-06 23:19:19 +1300

    update webpacker config

--- webpack.config.js
+++ webpack.config.js
@@ -4,60 +4,66 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
-   filename: 'main.css'
+  filename: 'main.css'
 });
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015']
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: extractPlugin.extract({
-                    use: ['css-loader', 'sass-loader']
-                })
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            },
-            {
-                test: /\.(jpg|png|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'img/',
-                            publicPath: 'img/'
-                        }
-                    }
-                ]
-            },
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015']
+            }
+          }
         ]
-    },
-    plugins: [
-        extractPlugin,
-        new HtmlWebpackPlugin({
-            filename: 'app.html',
-            template: 'src/app.html'
-        }),
-        new CleanWebpackPlugin(['dist'])
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/'
+            }
+          }
+        ]
+      },
     ]
+  },
+  plugins: [
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      filename: 'app.html',
+      template: 'src/app.html'
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: {
+      index: '/dist/app.html'
+    }
+  }
 };
